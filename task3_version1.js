3eaa5d256bf108cd4e79ec4d71c8a226b834cb31
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-22 22:45:28 +0700

    fix: fix problem for sort loop

--- task3_version1.js
+++ task3_version1.js
@@ -5,34 +5,15 @@ function divideAndSort(num){
     let temp = 0
     let res = []
     for(let i = 0; i < lg; i++){
-        arr.push(numStr[i])
-        // if(numStr[i]==0){
-        //     arr.push(temp)
-        //     temp=""
-        // } else {
-        //     temp += numStr[i]
-        // }
+        
+        if(numStr[i]==0){
+            arr = [...`${temp}`]
+            temp=""
+        } else {
+            temp += numStr[i]
+        }
     }
 
-    // if(temp){
-    //     arr.push(temp)
-    // }
-
-    // for(let a = 0; a<arr.length; a++){
-    //     res.push( arr[a])
-    //     for(let b=0; b<res.length; b++){
-    //        if(res[b]!=0){
-    //         if (res[b] > res[b + 1]){
-    //             let x = res[b];
-    //             res[b] = res[b + 1];
-    //             res[b + 1] = x;
-    //             b = -1;   
-        
-    //         }
-    //        }
-            
-    //     }
-    // }
     for (let j = 0; j < arr.length - 1; j++){
         if(arr[j]!=0){
             if (arr[j] > arr[j + 1]){
@@ -44,6 +25,7 @@ function divideAndSort(num){
             }
         
         }
+        console.log(arr[j])
     }
     return arr
         
