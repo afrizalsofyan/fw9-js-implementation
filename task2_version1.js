af1824a907112bc203554c478eb61af367e8cffc
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-23 15:52:51 +0700

    fix: revisi manual reverse loop

--- task2_version1.js
+++ task2_version1.js
@@ -1,31 +1,35 @@
-const words = "Saya belajar Javascript" 
+const words = "Saya belajar Javascript disini" 
 let res = ""
 let temp = ""
 let arr = []
+let arrLast = []
+let arrNew = []
 let i = 0
 let x = words.length-1
 let a = 0
+let j = 0
 
 while(i<=x){
-    if(words[i]==" "){
-        arr.push(temp)
-        temp = ""
-    } else{
-        temp += words[i]
+
+    if(words[i]==0){
+        arr[a]=temp
+        a++
+        temp=""
+    } else {
+        temp+=words[i] 
     }
+
+    if(i==x){
+        arrLast=temp
+    }
+
     i++
 }
 
-if(temp){
-    arr.push(temp)
+arrNew = [...arr, arrLast]
+let y =arrNew.length-1
+let temp2
+for(j=y; j>=0;j--){
+    res += arrNew[j] + " "
 }
-
-a = arr.length-1
-while(a>=0){
-    // console.log(arr[a])
-    res += arr[a]+" "
-    a--
-}
-
-console.log(res)
-
+console.log(res);
